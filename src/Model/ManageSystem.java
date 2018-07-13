ae78a245ee47486599f08819b15a3bb1fdeb90a7
Yiheng Shu <lzw429@qq.com>
2018-07-13 22:20:29 +0800

    update

--- src/Model/ManageSystem.java
+++ src/Model/ManageSystem.java
@@ -102,6 +102,9 @@ public class ManageSystem {
                 case 9:
                     manageSystem.ParkingLotManage();
                     break;
+                case 10: // 回溯搜索哈密尔顿回路，不保证最短
+                    manageSystem.HamCycle();
+                    break;
                 default:
                     System.out.println("[错误]请重新选择");
                     break;
@@ -122,6 +125,7 @@ public class ManageSystem {
         System.out.println("7.两景点最短路径与最短距离"); // Dijkstra 算法和 FloydWarshall 算法
         System.out.println("8.导游路线图");
         System.out.println("9.停车场管理");
+        System.out.println("10.搜索回路");
         System.out.println("=============================");
         System.out.println("请选择：");
     }
@@ -598,5 +602,115 @@ public class ManageSystem {
 
         List<String> traversal = rootNode.preorderTraversal();
         path.addAll(traversal);
+    }
+
+    // 通过全排列和回溯可搜索非完全图的最短哈密尔顿回路，但效率较低；
+    // 对于12个顶点的无向带权图，需要39916880次回溯才能找到指定起点的最短哈密尔顿回路
+
+    /**
+     * @param str 被全排列的字符串
+     * @param pos 当前位置
+     * @param len 排列范围是[pos, pos + len)
+     */
+    public void nextPermutation(String[] str, int pos, int len) {
+        if (pos == len - 1) {
+
+        } else {
+            for (int i = pos; i < len; i++) {
+                Util.Util.swap(str, pos, i);
+                nextPermutation(str, pos + 1, len);
+                Util.Util.swap(str, pos, i);
+            }
+        }
+    }
+
+    private int backTrackingCount = 0;
+
+    /**
+     * 用于 Hamiltonian 环的递归函数
+     *
+     * @param hamCycle 正在生成的Hamiltonian图
+     * @param pos      当前hamCycle数组的索引
+     * @return 找到解返回true，否则false
+     */
+    private boolean hamCycleUtil(int pos, String[] hamCycle, int minLen) {
+        // 如果所有顶点都被添加到Hamilton环
+        if (pos == spots.size()) {
+            // 如果最后一个顶点与首个顶点间有边
+            Model.ArcNode arc = getArc(hamCycle[pos - 1], hamCycle[0]);
+            if (arc != null) {
+                minLen += arc.getDistance();
+                System.out.println("最短环游长度：" + minLen + " " + (backTrackingCount++));
+                for (int i = 0; i < hamCycle.length; i++)
+                    System.out.print(hamCycle[i] + "->");
+                System.out.println(hamCycle[0]);
+                return true;
+            }
+            return false;
+        }
+        // 在Hamilton环中尝试不同的候选顶点，跳过0因为开始时已添加
+        for (Map.Entry<String, Model.VNode> entry : spots.entrySet()) {
+            String curSpot = entry.getKey();
+            if (isSafe(curSpot, hamCycle, pos)) {
+                hamCycle[pos] = curSpot;
+                int dist = getArc(curSpot, hamCycle[pos - 1]).getDistance();
+                minLen += dist;
+                // 递归构建剩余路径
+                if (hamCycleUtil(pos + 1, hamCycle, minLen))
+                    return true;
+                // 如果添加顶点curSpot找不到解，去除
+                minLen -= dist;
+                hamCycle[pos] = null;
+            }
+        }
+        // 如果没有顶点能被添加到现有的环中，返回false
+        return false;
+    }
+
+    /**
+     * 全排列回溯暴力搜索最短Hamiltonian回路
+     */
+    public void hamCyclePermutation(String[] hamCycle, int pos, int len) {
+        int minLenOfAll = Integer.MAX_VALUE;
+        if (pos == len - 1) {
+            int minLen = 0;
+            hamCycleUtil(1, hamCycle, minLen);
+            if (minLen < shortestTourLen)
+                shortestTourLen = minLen;
+        } else {
+            for (int i = pos; i < len; i++) {
+                Util.Util.swap(hamCycle, pos, i);
+                hamCyclePermutation(hamCycle, pos + 1, len);
+                Util.Util.swap(hamCycle, pos, i);
+            }
+        }
+    }
+
+    /**
+     * 检查顶点curSpot是否可在hamCycle[pos]处添加
+     *
+     * @param curSpot  当前顶点
+     * @param hamCycle 正在生成的Hamiltonian图
+     * @param pos      当前hamCycle数组的索引
+     * @return 可添加true，否则false
+     */
+    private boolean isSafe(String curSpot, String[] hamCycle, int pos) {
+        // 判断该顶点是否前一顶点的邻接顶点
+        if (getArc(hamCycle[pos - 1], curSpot) == null)
+            return false;
+        // 检查顶点是否被包含在hamCycle中
+        for (int i = 0; i < pos; i++)
+            if (hamCycle[i].equals(curSpot))
+                return false;
+        return true;
+    }
+
+    private void HamCycle() {
+        String[] hamCycle = new String[spots.size()];
+        int i = 0;
+        for (Map.Entry<String, VNode> entry : spots.entrySet()) {
+            hamCycle[i++] = entry.getKey();
+        }
+        hamCyclePermutation(hamCycle, 1, hamCycle.length);
     }
 }
